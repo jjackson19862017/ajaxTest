061126aaf8ed3dbd09804061ba94a0523ebf7173
Stephen Jackson <jjackson19862017@gmail.com>
2019-09-11 11:46:03 +0100

    Timeout Function

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 var xhr = new XMLHttpRequest();
+var data;
 
 xhr.open("GET", "https://swapi.co/api/");
 xhr.send();
 
+function setData(jsonData) {
+    data = jsonData;
+}
+
 xhr.onreadystatechange = function() {
+    console.log(this.readyState);
     if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("data").innerHTML = this.responseText;
+        setData(JSON.parse(this.responseText));
     }
 };
+
+setTimeout(function() {
+    console.log(data);
+}, 1500);
