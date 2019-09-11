8f20e573f265d23884f372d8667d2e13b4a075be
Stephen Jackson <jjackson19862017@gmail.com>
2019-09-11 11:50:07 +0100

    Changed Timeout to 500ms

--- main.js
+++ main.js
@@ -4,17 +4,12 @@ var data;
 xhr.open("GET", "https://swapi.co/api/");
 xhr.send();
 
-function setData(jsonData) {
-    data = jsonData;
-}
-
 xhr.onreadystatechange = function() {
-    console.log(this.readyState);
     if (this.readyState == 4 && this.status == 200) {
-        setData(JSON.parse(this.responseText));
+        data = JSON.parse(this.responseText);
     }
 };
 
 setTimeout(function() {
     console.log(data);
-}, 1500);
+}, 500);
