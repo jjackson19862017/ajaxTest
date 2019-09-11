8b85273c9d4f17c1a3956ac74e19250a6aa0f77c
Stephen Jackson <jjackson19862017@gmail.com>
2019-09-11 11:54:28 +0100

    Error Correction

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ function getData(cb) {
 
     xhr.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            cbJSON.parse(this.responseText);
+            cb(JSON.parse(this.responseText));
         }
     };
 }
