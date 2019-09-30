b4e1777ed0db4bc890370c5c427914562d2501fd
Stephen Jackson <jjackson19862017@gmail.com>
2019-09-30 13:27:26 +0100

    Doing my head in

--- main.js
+++ main.js
@@ -3,14 +3,14 @@ const baseURL = "https://swapi.co/api/";
 function getData(type, cb) {
     var xhr = new XMLHttpRequest();
 
-    xhr.open("GET", baseURL + type + "/");
-    xhr.send();
-
     xhr.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             cb(JSON.parse(this.responseText));
         }
     };
+
+    xhr.open("GET", baseURL + type + "/");
+    xhr.send();
 }
 
 function getTableHeaders(obj) {
@@ -20,7 +20,7 @@ function getTableHeaders(obj) {
         tableHeaders.push(`<td>${key}</td>`);
     });
 
-    return `<tr>${tableHeaders}</td>`;
+    return `<tr>${tableHeaders}</tr>`;
 }
 
 function writeToDocument(type) {
@@ -29,18 +29,19 @@ function writeToDocument(type) {
     el.innerHTML = "";
 
     getData(type, function(data) {
-        data = data.results
+        data = data.results;
         var tableHeaders = getTableHeaders(data[0]);
 
         data.forEach(function(item) {
             var dataRow = [];
             Object.keys(item).forEach(function(key) {
-                dataRow.push(`<td>{$item[key]}</td>`);
+                var rowData = item[key].toString();
+                var truncatedData = rowData.substring(0, 15)
+                dataRow.push(`<td>${truncatedData}</td>`);
             });
-            tableRows.push(dataRow);
-        })
+            tableRows.push(`<tr>${dataRow}</tr>`);
+        });
 
-        // el.innerHTML += "<p>" + item.name + "</p>";
+        el.innerHTML = `<table>${tableHeaders}</table>`;
     });
-    el.innerHTML = `<table>${tableHeaders}${tableRows}</table>`;
-});
+}
